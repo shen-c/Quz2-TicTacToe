0bfe0213dd6771a3abeed2e27b587e790ffb2a79
shen-c <congshen@cpp.edu>
2022-10-07 19:24:16 -0700

    file with only comments, initial commit

---
+++ tictactoe.c
@@ -0,0 +1,11 @@
+//display the board
+
+//ask for user input and place onto board
+
+//validate if move is legal
+
+//generate a random move by computer
+
+//check win conditions
+
+//main execution
